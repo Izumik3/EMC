d9a80d1f63ae4078f6594abe67c9b25ed3326716
romaw <poctawsky20@yahoo.com>
2026-03-19 02:52:56 +0300

    Fix linting issues

--- app/src/app/components/Header.jsx
+++ app/src/app/components/Header.jsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export default function Header() {
   return (
-    <header className="w-full bg-gradient-to-b from-[#FFFFFF] to-[#D8D8D8] p-4  font-light">
+    <header className="w-full bg-gradient-to-b from-[#FFFFFF] to-[#D8D8D8] p-4 font-light">
       <div className="max-w-content w-[90%] mx-auto flex items-center justify-between">
-        <a href="/"><div className="text-[24px]">ЭЛЕКТРОННЫЙ МАСТЕР-КОНСУЛЬТАНТ</div></a>
-        
+        <Link href="/">
+          <div className="text-[24px]">ЭЛЕКТРОННЫЙ МАСТЕР-КОНСУЛЬТАНТ</div>
+        </Link>
+
         <button className="w-8 h-6 flex flex-col justify-between">
           <span className="h-1 bg-black rounded"></span>
           <span className="h-1 bg-black rounded"></span>
